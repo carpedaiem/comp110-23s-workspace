92342d92fcc6a62801ba3cbf7b2c80447abd7348
Emily Dai <emiillie@ad.unc.edu>
2023-02-02 20:03:29 -0500

    Adjusted code

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -1,29 +1,29 @@
-"""EX 02 - One shot Wordle"""
+"""EX 02 - One shot Wordle!"""
 __author__ = "730374002"
 
-secret = "knoll"
+secret = "python"
 
 """ Length of secret word"""
 length = len(secret)
 
-guess: str=input("What is your " + str(length) + "-letter guess? ")
+guess: str = input("What is your " + str(length) + "-letter guess? ")
 
 while len(guess) != length:
-    guess: str=input("That was not " + str(length)  + " letters! Try again: ")
+    new_guess: str = input("That was not " + str(length) + " letters! Try again: ")
+    guess = new_guess
 
 WHITE_BOX: str = "\U00002B1C"
 GREEN_BOX: str = "\U0001F7E9"
 YELLOW_BOX: str = "\U0001F7E8"
 
 letter = 0
-
 boxes = ""
 
 while letter < length:
     guess_check = guess[letter]
     secret_check = secret[letter]
     if ord(guess_check) == ord(secret_check):
-        boxes = boxes + GREEN_BOX + " "
+        boxes = boxes + GREEN_BOX
     else: 
         yellow_find = False
         yellow_length = 0
@@ -31,10 +31,10 @@ while letter < length:
             if ord(guess_check) == ord(secret[yellow_length]):
                 yellow_find = True
             yellow_length = yellow_length + 1
-        if yellow_find == True:
-            boxes = boxes + YELLOW_BOX + " "
+        if yellow_find is True:
+            boxes = boxes + YELLOW_BOX
         else: 
-            boxes = boxes + WHITE_BOX + " "
+            boxes = boxes + WHITE_BOX
     letter = letter + 1
 
 print(boxes)
